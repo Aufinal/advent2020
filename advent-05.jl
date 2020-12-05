fbbcbb8a67ff2548847b029617cf36374804113e
Ludovic Stephan <ludovic.stephan@ens.fr>
2020-12-05 14:43:54 +0100

    Golfing 5

--- advent-05.jl
+++ advent-05.jl
@@ -1,10 +1,5 @@
-function parse_bin(str)
-    str = map(c -> c == 'B' || c == 'R' ? '1' : '0', str)
-    return parse(Int, str, base=2)
-end
-
 open("input-05") do file
-    ids = Set(map(parse_bin, eachline(file)))
+    ids = Set(map(str -> parse(Int, map(c -> c == 'B' || c == 'R' ? '1' : '0', str), base=2), eachline(file)))
     # Problem 1
     println("Maximum ID : $(maximum(ids))")
     # Problem 2
