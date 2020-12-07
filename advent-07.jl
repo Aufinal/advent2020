e8ce39d3daa1c5f7d20ea9e67b5c36f7f349408b
Ludovic Stephan <ludovic.stephan@ens.fr>
2020-12-07 15:47:46 +0100

    Better complexity

--- advent-07.jl
+++ advent-07.jl
@@ -1,11 +1,9 @@
-using Memoize
-
 struct Child
     num::Int
     color::String
 end
 
-function parse_file(filename)
+function parse_file(filename::String)::Dict{String,Array{Child}}
     tree = Dict{String,Array{Child}}()
     open(filename) do file
         for line in eachline(file)
@@ -19,18 +17,26 @@ function parse_file(filename)
     return tree
 end
 
-@memoize function ancestor(color, tree, node)
-    if node == color
-        return true
-    elseif node ∉ keys(tree)
-        return false
-    else
-        return any(ancestor(color, tree, child.color) for child in tree[node])
+function sum_contains(tree::Dict{String,Array{Child}}, target::String)
+    contains = Dict{String,Bool}()
+    function aux!(node::String, contains::Dict{String,Bool}, target::String, tree::Dict{String,Array{Child}})::Bool
+        if node == target
+            return true
+        elseif haskey(contains, node)
+            return contains[node]
+        elseif !haskey(tree, node)
+            return false
+        else
+            result = any(aux!(child.color, contains, target, tree) for child in tree[node])
+            contains[node] = result
+            return result
+        end
     end
+    return sum(aux!(key, contains, target, tree) for key in keys(tree))
 end
 
-function contains(tree, node)
-    if node ∉ keys(tree)
+function contains(tree::Dict{String,Array{Child}}, node::String)::Int
+    if !haskey(tree, node)
         return 0
     else
         return sum(child.num * (1 + contains(tree, child.color)) for child in tree[node])
@@ -40,6 +46,6 @@ end
 println("Parsing...")
 @time tree = parse_file("input-07") 
 println("Problem 1 :")
-@time println(sum(ancestor("shiny gold", tree, node) for node in keys(tree)))
+@time println(sum_contains(tree, "shiny gold"))
 println("Problem 2 :")
 @time println(contains(tree, "shiny gold"))
