eed362b15c21db218a5e9c7ed1465ef9c7b1b48f
Ludovic Stephan <ludovic.stephan@ens.fr>
2020-12-15 11:24:38 +0100

    Optimize day 15

--- advent-15.jl
+++ advent-15.jl
@@ -1,24 +1,22 @@
-open("input-15") do file
-    numbers = [parse(Int, num) for num in split(readline(file), ",")]
-    last_spoken = Dict{Int,Int}()
-    
+function run_game(numbers::Array{UInt32,1}, n_steps::Int)
+    last_spoken = Dict{UInt32,UInt32}()
+
     for (i, n) in enumerate(numbers)
         last_spoken[n] = i
     end
 
     spoken = 0
-    for turn_number in 1 + length(numbers):30000000 - 1
-        if turn_number == 2020
-            println(spoken)
-        end
-        if haskey(last_spoken, spoken)
-            l = last_spoken[spoken]
-            last_spoken[spoken] = turn_number
-            spoken = turn_number - l
-        else
-            last_spoken[spoken] = turn_number
-            spoken = 0
-        end
+    for turn_number in 1 + length(numbers):n_steps - 1
+        last_spoken[spoken], spoken = turn_number, turn_number - get(last_spoken, spoken, turn_number)
     end
-    println(spoken)
+
+    return spoken
+end
+
+open("input-15") do file
+    numbers = [parse(UInt32, num) for num in split(readline(file), ",")]
+    @time res1 = run_game(numbers, 2020)
+    @time res2 = run_game(numbers, 30000000)
+
+    println("$res1 $res2")
 end
