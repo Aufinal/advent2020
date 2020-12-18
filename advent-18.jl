8e9fa1b0cf4ccdfa82faecb3b2d9dc38941996fb
Ludovic Stephan <ludovic.stephan@ens.fr>
2020-12-18 21:06:14 +0100

    Julia program that works

--- advent-18.jl
+++ advent-18.jl
@@ -1,6 +1,6 @@
 ⊗, ⊕ = +, *
 open("input-18") do file
-    lines = readlines(file)
-    println(sum(map(eval ∘ Meta.parse, replace.(lines, "*" => "⊕"))))
-    println(sum(map(eval ∘ Meta.parse, replace.(lines, "*" => "⊕", "+" => "⊗"))))
+    lines = replace.(readlines(file), "*" => "⊕")
+    @time println(sum(map(eval ∘ Meta.parse, lines)))
+    @time println(sum(map(eval ∘ Meta.parse, replace.(lines, "+" => "⊗"))))
 end
