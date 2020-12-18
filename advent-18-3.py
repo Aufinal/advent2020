4945cd91430acb324f333391bc41a4dfc77c9692
Ludovic Stephan <ludovic.stephan@ens.fr>
2020-12-18 21:42:09 +0100

    Day18 using ast

---
+++ advent-18-3.py
@@ -0,0 +1,17 @@
+from ast import parse, walk, BinOp, Mult, Sub, BitOr
+
+
+def mod_expr(expr, token, opclass):
+    e = expr.replace("*", token)
+    a = parse(e, mode="eval")
+    for node in walk(a):
+        if type(node) == BinOp and type(node.op) == opclass:
+            node.op = Mult()
+
+    return eval(compile(a, "", mode="eval"))
+
+
+with open("input-18", "r") as f:
+    lines = f.readlines()
+    print(sum(mod_expr(line, "-", Sub) for line in lines))
+    print(sum(mod_expr(line, "|", BitOr) for line in lines))
