d693db42f10d074a1c1b265658274bdc63be503f
Ludovic Stephan <ludovic.stephan@ens.fr>
2020-12-18 14:04:58 +0100

    Simplify code

--- advent-18.jl
+++ advent-18.jl
@@ -1,6 +1,6 @@
 ⊗, ⊕ = +, *
 open("input-18") do file
-    lines = replace.(readlines(file), "*" => "⊕")
-    println(sum(map(eval ∘ Meta.parse, lines)))
-    println(sum(map(eval ∘ Meta.parse, replace.(lines, "+" => "⊗"))))
+    lines = readlines(file)
+    println(sum(map(eval ∘ Meta.parse, replace.(lines, "*" => "⊕"))))
+    println(sum(map(eval ∘ Meta.parse, replace.(lines, "*" => "⊕", "+" => "⊗"))))
 end
